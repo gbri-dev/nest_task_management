3d1de9b0e3a74fd7ecc6e08ac22d45b62824d6bf
gbridev <gabriel.camposdasilva@hotmail.com>
2025-04-29 13:33:57 -0300

    feat(typeorm e docker): adicionando o banco de dados postgres e rodando as migrations e gerando as tabelas no banco de dados.

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,10 +1,13 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
-import { FindAllParameters, TaskDto } from './task.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { FindAllParameters, TaskDto, TaskStatusEnum } from './task.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TaskService {
     private tasks: TaskDto[] = [];
     create(task: TaskDto) {
+        task.id = uuid()
+        task.status = TaskStatusEnum.TO_DO
         this.tasks.push(task)
     }
     findById(id: string): TaskDto {
